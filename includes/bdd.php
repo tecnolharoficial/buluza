2842a1087a5330ec528af984db9deb8115f546d8
Tecnolhar <75804329+tecnolharoficial@users.noreply.github.com>
2023-10-03 13:40:16 -0300

    Update bdd.php

--- includes/bdd.php
+++ includes/bdd.php
@@ -283,7 +283,7 @@
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>Buluza</title>
+            <title>Gata da Área</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-proxima-nova@1.0.1/style.min.css">
@@ -443,7 +443,7 @@
                 <hr class="mt-2 mb-2">
                 <div class="row">
                     <div class="col-md-12">
-                        <p class="text-center m-0">Copyright © 2023 <a href="#">Ask</a>. Todos os direitos reservados.</p>
+                        <p class="text-center m-0">Copyright © 2023 <a href="<?php echo PATH; ?>">Gata da Área</a>. Todos os direitos reservados.</p>
                     </div>
                 </div>
             </div>
