626b5e9f6939f953394c6dd655b92062a338372e
Tecnolhar <75804329+tecnolharoficial@users.noreply.github.com>
2023-12-04 08:30:06 -0300

    Update bdd.php

--- includes/bdd.php
+++ includes/bdd.php
@@ -13,7 +13,7 @@
 
     //Conexão com o banco principal
     try {
-        $bdd = new PDO('mysql:host='.HOST.';dbname=look9131_gatadaarea;charset=utf8', 'look9131_gatadaarea', 'xX40028922');
+        $bdd = new PDO('mysql:host='.HOST.';dbname=tecnolha_gatadaarea;charset=utf8', 'tecnolha_gatadaarea', 'xX40028922');
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch(PDOException $error) {
